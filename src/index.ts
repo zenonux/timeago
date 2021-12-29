ed7eb60f7fcf5f011fb9d88e172f3fee88514e60
zenonux@@outlook.com <zenonux@@outlook.com>
2021-12-29 13:51:24 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ export default function format(time: string | number | Date) {
   else if (hour >= 1) result = Math.floor(hour) +'小时前'
   else if (minute >= 1) result =  Math.floor(minute) +'分钟前'
   else if (second >= 9) result = Math.floor(second) +'秒前'
-  else if (second >= 1) result = '刚刚'
+  else  result = '刚刚'
   return result
 }
 
